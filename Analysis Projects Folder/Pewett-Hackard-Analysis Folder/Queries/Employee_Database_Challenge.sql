e3a2719cef3f0e0b19f3430dc331fdfa3fbc2ee1
Andrew Abramo <drewabramo12@gmail.com>
2022-02-20 23:46:21 -0800

    Completed

--- Analysis Projects Folder/Pewett-Hackard-Analysis Folder/Queries/Employee_Database_Challenge.sql
+++ Analysis Projects Folder/Pewett-Hackard-Analysis Folder/Queries/Employee_Database_Challenge.sql
@@ -1,5 +1,5 @@
 -- Queries for _PH_EmployeeDB.sql
-
+--Deliverable 1
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
 INTO retirement_titles
 FROM employees AS e
@@ -24,3 +24,35 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT DESC;
+
+-- Deliverable 2
+-- mentorship_eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
+
+-- Additional queries
+SELECT COUNT(title), title
+INTO mentor_count
+FROM mentorship_eligibilty
+GROUP BY title
+ORDER BY COUNT DESC;
+
+SELECT COUNT(emp_no)
+FROM employees;
+
+SELECT SUM(count)
+FROM retiring_titles;
